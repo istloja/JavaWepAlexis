9a34ee7ab18a15710230f262c7de8894e1fc98ca
AlexisO <AlexisO@192.168.137.119>
2019-12-11 15:59:27 -0500

    Sincronizacion Inicial

---
+++ ProgramacionWepAlexis/src/java/beans/BeanLogin.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author AlexisO
+ */
+@Named(value = "beanLogin")
+@RequestScoped
+public class BeanLogin implements Serializable  {
+
+    
+    private String usuario, contrasenia;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+   
+    public void comprobar() {
+        FacesMessage mensaje;
+        if (usuario.equals("alexis") && contrasenia.equals("123456")) {
+            System.out.println("Datos Correctos");
+            mensaje= new FacesMessage(FacesMessage.SEVERITY_INFO,"Datos correctos"," ");
+        } else {
+            System.out.println("Datos Incorrectos");
+            mensaje= new FacesMessage(FacesMessage.SEVERITY_INFO,"Datos Incorrectos","");
+        }
+        FacesContext.getCurrentInstance().addMessage(null, mensaje);
+    }
+
+    }
+
+    
+    
+    
+
